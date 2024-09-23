f729f5dc83068b8c5d27206ae42cfedcb8d890af
Kate Doxey <kedoxey@gmail.com>
2024-09-23 12:05:35 -0700

    match notebook to script

--- temp.py
+++ temp.py
@@ -31,3 +31,23 @@ axs[-1].set_xlabel('Time (ms)')
 
 fig.tight_layout()
 fig.savefig(os.path.join(cwd, 'output', 'voltage_traces.png'), dpi=300)
+
+# notebook
+fig, axs = plt.subplots(5, 1, figsize=(8,13))
+axs.ravel()
+
+t = simData['t']
+
+for cell_id, v_soma in simData['V_soma'].items():
+    if 'dict' not in cell_id:
+        gid = int(cell_id.split('_')[1])
+        cell_pop = cells[gid].tags['pop']
+        axs[gid].plot(t, v_soma, label=cell_pop, color=colors[cell_pop])
+        axs[gid].set_title(f'{cell_pop}')
+    axs[gid].set_ylim([-75,45])
+    axs[gid].set_yticks([-60, -30, 0, 30])
+    axs[gid].set_ylabel('Voltage (mV)')
+
+axs[-1].set_xlabel('Time (ms)')
+
+fig.tight_layout()
