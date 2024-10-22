71b2cd008e4566b6d4d959efe387a8904ec1f637
Kate Doxey <kedoxey@gmail.com>
2024-10-22 11:26:10 -0700

    simulations for parameterization and fully connected network

--- batch_network_netpyne.py
+++ batch_network_netpyne.py
@@ -4,16 +4,16 @@ from sklearn.model_selection import ParameterGrid
 
 config_name = 'default_config'
 
-paramGrid = {'sim_name': ['in_to_fusi'],
-             'in_amp': [0],
-             'sf_exc_gmax': [0],
-             'fin_exc_gmax': [0],
-             'fic_exc_gmax': [0],
-             'icf_exc_gmax': [0],
-             'icin_exc_gmax': [0],
+paramGrid = {'sim_name': ['fully connected'],
+             'in_amp': [0.55],
+             'sf_exc_gmax': [0.19],
+             'fin_exc_gmax': [0.016],
+             'fic_exc_gmax': [0.05],
+             'icf_exc_gmax': [0.05],
+             'icin_exc_gmax': [0.05],
              'inf_inh_gmax': [0.0045],
-             'enable_loss': [False],
-             'enable_IC': [True]}
+             'enable_loss': [True, False],
+             'enable_IC': [True, False]}
 batchParamsList = list(ParameterGrid(paramGrid))
 
 thresh_params = []
